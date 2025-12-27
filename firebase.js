f4c01546b59cc174f8e93c767910e8ff55069bf6
Ipannazim <irfanfairuzazim@gmail.com>
2025-12-28 01:33:48 +0800

    Remove backend files for Vercel static deployment

--- firebase.js
+++ firebase.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyBQ-6U0sWJLI8f-Bfqgnm-JU-mcgj2en8Q",
     authDomain: "calorie-tracker-826bd.firebaseapp.com",
@@ -19,5 +16,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
-export { db };
+export { app, db, analytics };
